6b135da64fc83efc86f5d4df4eeeeb5224c86057
naciye-aydn <naciye.aydin@boun.edu.tr>
2022-09-28 22:42:59 +0300

    metot1-2

---
+++ soru/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan= new Scanner(System.in);
+
+    public static String metot1(){
+        System.out.println("kullanıcın cinsiyetini giriniz");
+        String kız_erkek= scan.next();
+        return kız_erkek;
+    }
+    public static int metot2(){
+        System.out.println("kullanıcının yaşını giriniz");
+        int yaş= scan.nextInt();
+        return yaş;
+    }
+
+    //Programcı 1 :  Aşağıda verilen kodları yazar ve githuba upload eder.
+    //Metot P1-1 : Bir metot yaz bu metot bizden erkek mi kız mı olduğumuz bilgisini alsın ve metot bu bilgiyi döndürsün.
+    //Metot P1-2: Bir metot yaz bu metot bizden yaşımızın kaç olduğu bilgisini alsın ve metot bu bilgiyi döndürsün. (MAX 40 , MİN 20)
+    public static void main(String[] args) {
+
+        
+    }
+}
